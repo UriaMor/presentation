67ad43c7dc68b2c8e9d4840c72535a6ab03a2a6c
Uria <uria@imrapid.com>
2015-11-27 16:57:05 +0200

    init

---
+++ server.js
@@ -0,0 +1,35 @@
+"use strict";
+
+
+
+var express = require('express');
+var path = require('path');
+
+var app = express();
+var server = require('http').Server(app);
+
+
+app.use('/', express.static(__dirname + '/static'));
+
+app.all('*', function(req, res, next) {
+       res.header("Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Headers", "X-Requested-With");
+       res.header('Access-Control-Allow-Headers', 'Content-Type');
+       next();
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+server.listen(5000);
